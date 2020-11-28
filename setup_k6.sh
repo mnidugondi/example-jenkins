235d156af965a094a96ed13864752cd9dda60660
mnidugondi <61990734+mnidugondi@users.noreply.github.com>
2020-11-28 17:57:16 -0500

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -6,4 +6,4 @@ echo "no sudo"
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 #echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
 #apt-get update
-brew install k6
+brew reinstall k6
