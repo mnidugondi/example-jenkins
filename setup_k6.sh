d3df5b5e2b9797e8c1234194782776f4453a6b04
mnidugondi <61990734+mnidugondi@users.noreply.github.com>
2020-11-28 17:55:03 -0500

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -4,6 +4,6 @@ echo "no sudo"
 #apt-get update
 #apt-get install dirmngr --install-recommends
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
+#echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
 #apt-get update
 brew install k6
