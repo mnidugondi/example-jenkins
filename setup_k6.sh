310f45ac6cb17a0b824635c44b435d1ea48e7547
mnidugondi <61990734+mnidugondi@users.noreply.github.com>
2020-11-28 21:11:41 -0500

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=$PATH:/usr/local/bin
+PATH=$PATH:/usr/local/bin:/usr/local/Homebrew/bin
 echo "path: "
 echo $PATH
 set -ex
@@ -9,4 +9,4 @@ echo "no sudo"
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 #echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
 #apt-get update
-/usr/local/bin/brew reinstall k6
+brew reinstall k6
